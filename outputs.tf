4796e4e255095e36d5477ff3cb3e760e67348869
Nosa Omorodion <japhet.omorodion@.com>
2021-06-19 15:17:00 +0100

    Renamed output file and added outputs

---
+++ outputs.tf
@@ -0,0 +1,21 @@
+output "public_dns_name" {
+  description = "Public DNS names of the load balancer for this project"
+  value       = module.elb_http.this_elb_dns_name
+}
+
+output "vpc_id" {
+  description = "ID of project VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "lb_url" {
+  description = "URL of load balancer"
+  value       = "http://${module.elb_http.this_elb_dns_name}/"
+}
+
+output "web_server_count" {
+  description = "Number of web servers provisioned"
+  value       = length(module.ec2_instances.instance_ids)
+}
+
+
